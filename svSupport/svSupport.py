ccf0159b44d4d7c8dd851533533e6ac91f0fe1d2
nriddiford <nickriddiford@me.com>
2018-01-19 14:00:42 +0100

    Remove intermediate files

--- svSupport/svSupport.py
+++ svSupport/svSupport.py
@@ -131,11 +131,15 @@ def bp_2_opposing_reads(bamFile, chrom, bp1, bp2, slop):
     return(bp2_reads, count)
 
 def merge_bams(out_file, bams):
-    # out_file = os.path.join(out_dir, out_file)
     in_files = ', '.join(bams)
     print("Merging bam files '%s' into '%s'") % (in_files, out_file)
     merge_parameters = ['-f', out_file] + bams
     pysam.merge(*merge_parameters)
+    # Remove individual bp files
+    for bp_file in bams:
+        os.remove(bp_file)
+        os.remove(bp_file + ".bai")
+
     pysam.index(out_file)
 
 
