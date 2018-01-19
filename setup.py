ae6bc92df7dfd50ea3df5d5e0810e56b4115d31c
nriddiford <nickriddiford@me.com>
2018-01-19 11:19:37 +0100

    Package devel

---
+++ setup.py
@@ -0,0 +1,18 @@
+import sys
+
+from setuptools import setup
+
+__VERSION__ = '0.1'
+
+requirements = ['pysam']
+
+setup(name='svSupport',
+      version=__VERSION__,
+      description='Find allele frequency for structural variants',
+      url='https://github.com/nriddiford/svSupport',
+      author='Nick Riddiford',
+      author_email='nick.riddiford@curie.fr',
+      license='MIT',
+      packages=['svSupport'],
+      install_requires=requirements,
+      zip_safe=False)
