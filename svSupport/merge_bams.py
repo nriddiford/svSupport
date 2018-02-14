f3a28b7278c71c24d66d5935fe0e344a9dc8cf8d
nriddiford <nickriddiford@me.com>
2018-02-14 10:46:17 +0100

    Check if outfile

--- svSupport/merge_bams.py
+++ svSupport/merge_bams.py
@@ -5,7 +5,7 @@ def merge_bams(out_file, bams):
 
     for bam_file in bams:
         pysam.index(bam_file)
-        
+
     in_files = ', '.join(bams)
     print("Merging bam files %s into '%s'") % (in_files, out_file)
     merge_parameters = ['-f', out_file] + bams
@@ -15,4 +15,5 @@ def merge_bams(out_file, bams):
         os.remove(bp_file)
         os.remove(bp_file + ".bai")
 
-    pysam.index(out_file)
+    if out_file:
+        pysam.index(out_file)
