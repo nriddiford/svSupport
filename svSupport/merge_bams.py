dc6cc196029f4cca8cbc04e05ebc68fefc237b16
nriddiford <nickriddiford@me.com>
2018-02-14 14:14:20 +0100

    Comment out indexing

--- svSupport/merge_bams.py
+++ svSupport/merge_bams.py
@@ -3,17 +3,17 @@ import os
 
 def merge_bams(out_file, bams):
 
-    for bam_file in bams:
-        pysam.index(bam_file)
+    # for bam_file in bams:
+    #     pysam.index(bam_file)
 
     in_files = ', '.join(bams)
     print("Merging bam files %s into '%s'") % (in_files, out_file)
     merge_parameters = ['-f', out_file] + bams
     pysam.merge(*merge_parameters)
     # Remove individual bp files
-    for bp_file in bams:
-        os.remove(bp_file)
-        os.remove(bp_file + ".bai")
-
-    if out_file:
-        pysam.index(out_file)
+    # for bp_file in bams:
+    #     os.remove(bp_file)
+    #     os.remove(bp_file + ".bai")
+    #
+    # if out_file:
+    #     pysam.index(out_file)
