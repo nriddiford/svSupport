1b0a9d97866e78e7e74f7eafbf15f77d1c962583
nriddiford <nickriddiford@me.com>
2018-02-15 15:00:58 +0100

    Fix typo

--- script/run_svSupport.py
+++ script/run_svSupport.py
@@ -7,7 +7,7 @@ import re
 svs_file='/data/kdi_prod/project_result/948/01.00/Analysis/Analysis/svSupport/data/all_samples_2.txt'
 
 out_file = open('config.txt', 'w')
-headers = ['sample', 'event', 'type', 'bam', 'locus', 'purity', 'read_depth', 'length(Kb)', 'bp1_locus', 'bp2_locus', 'affected_genes']
+headers = ['sample', 'event', 'type', 'bam', 'locus', 'purity', 'read_depth', 'length(Kb)', 'bp1_locus', 'bp2_locus', 'affected_genes', 'guess']
 
 out_file.write('\t'.join(headers) + '\n')
 bams = '/data/kdi_prod/project_result/948/01.00/Analysis/Bwa'
@@ -31,16 +31,16 @@ with open(svs_file, 'r') as variants:
     for index, variant in dataset.iterrows():
         if variant['genotype'] != 'somatic_tumour':
             continue
-        if variant['type'] != 'DEL':
+        if variant['chromosome1'] != variant['chromosome2']:
             continue
-	
+
         ratio_search = variant['sample'] + '*_ratio.txt'
         bam_search = variant['sample'] + '.tagged.filt.SC.RG.bam'
 
         sample = variant['sample']
-	   
+
         m = re.search(r'(.*)R',sample)
-	
+
         if m:
             group = m.group(1)
 	else:
@@ -48,10 +48,10 @@ with open(svs_file, 'r') as variants:
 
 	if group == 'A373':
 	    group='A370'
-	
+
 	elif group == 'A573':
             group='A572'
-  
+
 	elif group == 'A785-A788':
 	     group = 'A785'
 
@@ -69,18 +69,19 @@ with open(svs_file, 'r') as variants:
         files = os.listdir(bam_data)
         depth_files = os.listdir(depth_data)
 
-        for name in files: 
+        for name in files:
             if fnmatch.fnmatch(name, bam_search):
                 bam_in = os.path.join(bam_data, name)
 
 	for name in depth_files:
     	    if fnmatch.fnmatch(name, ratio_search):
                 ratio_file = os.path.join(depth_data, name)
-        
+
         if "cnv" in variant['source'].lower():
             out_line = [variant['sample'], variant['event'], variant['type'],  bam_in, variant['position'], variant['purity'], ratio_file, variant['length(Kb)'], variant['bp1_locus'], variant['bp2_locus'], variant['affected_genes']]
+        elif variant['type'] != 'DEL':
+            out_line = [variant['sample'], variant['event'], variant['type'], bam_in, variant['position'], variant['purity'], 'NA', variant['length(Kb)'], variant['bp1_locus'], variant['bp2_locus'], variant['affected_genes'], 'T']
         else:
             out_line = [variant['sample'], variant['event'], variant['type'], bam_in, variant['position'], variant['purity'], 'NA', variant['length(Kb)'], variant['bp1_locus'], variant['bp2_locus'], variant['affected_genes']]
 
         out_file.write('\t'.join(map(str, out_line)) + '\n')
-        
