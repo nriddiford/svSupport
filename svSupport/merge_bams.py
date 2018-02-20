b31cff9d13e41aabd64628d2b2b58d53b7fe5d65
nriddiford <nickriddiford@me.com>
2018-02-20 09:58:39 +0100

    Update

--- svSupport/merge_bams.py
+++ svSupport/merge_bams.py
@@ -38,6 +38,6 @@ def merge_bams(out_file, out_dir, bams):
             print("Trying a shell call %s" % command)
             call(command, shell=True)
         except:
-            print("tried shell call: %s" % command)
+            print("Tried shell call in merge_bams: %s" % command)
             print("Can't index %s" % out_file)
             pass
