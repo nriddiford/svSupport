fe04d27387f4c1240f7b73edacbcb516cffc6962
nriddiford <nickriddiford@me.com>
2018-02-20 12:06:27 +0100

    Sort before index

--- svSupport/merge_bams.py
+++ svSupport/merge_bams.py
@@ -6,23 +6,26 @@ def merge_bams(out_file, out_dir, bams):
     s_bams = []
     for bam_file in bams:
         try:
-            name = os.path.splitext(bam_file)[0]
             head, file_name = ntpath.split(bam_file)
             sorted_bam = os.path.join(out_dir, file_name + ".s" + ".bam")
             pysam.sort("-o", sorted_bam, bam_file)
             os.remove(bam_file)
             s_bams.append(sorted_bam)
-        except:
-            print("Can't sort %s" % bam_file)
-        try:
             pysam.index(sorted_bam)
         except:
-            print("Can't index %s" % sorted_bam)
+            print("Can't sort %s" % bam_file)
 
     in_files = ', '.join(s_bams)
     print("Merging bam files %s into '%s'") % (in_files, out_file)
     merge_parameters = ['-f', out_file] + s_bams
     pysam.merge(*merge_parameters)
+
+    head, file_name = ntpath.split(out_file)
+    merged_bam = os.path.splitext(file_name)[0]
+
+    sorted_bam = os.path.join(out_dir, merged_bam + ".s" + ".bam")
+    pysam.sort("-o", sorted_bam, out_file)
+    pysam.index(out_file)
 
     #Remove individual bp files
     for bp_file in s_bams:
@@ -34,15 +37,15 @@ def merge_bams(out_file, out_dir, bams):
             print("Couldn't remove %s" % abs_file)
             pass
 
-    try:
-        pysam.index(out_file)
-    except pysam.utils.SamtoolsError:
-        print("Can't index %s" % out_file)
-        command = ' '.join(["samtools index ", out_file])
-        try:
-            print("Trying a shell call %s" % command)
-            call(command, shell=True)
-        except:
-            print("Tried shell call in merge_bams: %s" % command)
-            print("Can't index %s" % out_file)
-            pass
+    # try:
+    #     pysam.index(out_file)
+    # except pysam.utils.SamtoolsError:
+    #     print("Can't index %s" % out_file)
+    #     command = ' '.join(["samtools index ", out_file])
+    #     try:
+    #         print("Trying a shell call %s" % command)
+    #         call(command, shell=True)
+    #     except:
+    #         print("Tried shell call in merge_bams: %s" % command)
+    #         print("Can't index %s" % out_file)
+    #         pass
