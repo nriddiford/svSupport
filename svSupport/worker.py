66be491586c05decd2d68b164bd454ea7d2b28ad
nriddiford <nickriddiford@me.com>
2018-08-15 15:36:23 +0200

    Allow TRA and parse integrants

--- svSupport/worker.py
+++ svSupport/worker.py
@@ -10,7 +10,27 @@ from depthOps import get_depth
 from findBreakpoints import find_breakpoints
 from calculate_allele_freq import AlleleFrequency
 
-from merge_bams import merge_bams, sort_bam
+from merge_bams import merge_bams, sort_bam, rm_bams
+
+
+def getCooridinates(bps):
+    chrom1 = None
+    try:
+        chrom1, bp1, bp2 = re.split(':|-', bps)
+        chrom2 = chrom1
+    except ValueError:
+        pass
+
+    if not chrom1:
+        try:
+            chrom1, bp1, chrom2, bp2 = re.split(':|-', bps)
+        except ValueError:
+            pass
+
+    bp1 = int(bp1)
+    bp2 = int(bp2)
+
+    return chrom1, bp1, chrom2, bp2
 
 
 def worker(options):
@@ -21,12 +41,10 @@ def worker(options):
     purity = float(options.purity)
     find_bps = options.find_bps
 
-    chrom, bp1, bp2 = re.split(':|-', options.region)
-    bp1 = int(bp1)
-    bp2 = int(bp2)
+    chrom1, bp1, chrom2, bp2 = getCooridinates(options.region)
 
     if debug:
-        print_options(bam_in, normal, chrom, bp1, bp2, find_bps, debug, options.test, out_dir)
+        print_options(bam_in, normal, chrom1, bp1, bp2, find_bps, debug, options.test, out_dir)
 
     chroms = []
     if options.chromfile:
@@ -35,63 +53,59 @@ def worker(options):
 
 
     if options.config:
-        print("python svSupport.py -i %s -n %s -l %s:%s-%s -p %s -f %s -o %s -v %s") % (
-        bam_in, normal, chrom, bp1, bp2, purity, find_bps, out_dir, options.variants_out)
+        # print("python svSupport.py -i %s -n %s -l %s:%s-%s -p %s -f %s -o %s -v %s") % (
+        # bam_in, normal, chrom1, bp1, bp2, purity, find_bps, out_dir, options.variants_out)
+        print options
 
     if normal:
         print("* Calculating allele frequency from read depth file: %s" % bam_in)
-        opposing, supporting, adj_ratio = get_depth(bam_in, normal, chrom, bp1, bp2)
 
-        af = AlleleFrequency(opposing, supporting, purity, chrom)
+        opposing, supporting, adj_ratio = get_depth(bam_in, normal, chrom1, bp1, bp2)
+
+        af = AlleleFrequency(opposing, supporting, purity, chrom1)
         allele_frequency, adj_ratio = af.read_depth_af()
-        classify_cnv(chrom, adj_ratio)
+        cnv_type = classify_cnv(chrom1, adj_ratio)
 
-        return (chrom, bp1, bp2, allele_frequency)
+        return bp1, bp2, allele_frequency, cnv_type, None
 
-    bp_regions, slop = get_regions(bam_in, chrom, bp1, bp2, out_dir)
+    bp_regions, slop = get_regions(bam_in, chrom1, bp1, chrom2, bp2, out_dir, options)
 
     if options.find_bps:
         print "Guessing bp"
-
-        bp1_guess, bp1 = find_breakpoints(bp_regions, chrom, bp1, 'bp1', options)
-        bp2_guess, bp2 = find_breakpoints(bp_regions, chrom, bp2, 'bp2', options)
+        bp1_guess, bp1 = find_breakpoints(bp_regions, chrom1, bp1, 'bp1', options)
+        bp2_guess, bp2 = find_breakpoints(bp_regions, chrom2, bp2, 'bp2', options)
 
     seen_reads = defaultdict(int)
-    bp_sig = defaultdict(int)
 
     supporting = []
     opposing = []
 
-    bp1_split_reads, bp1_best_guess, bp1_clipped_bam, bp1_disc_bam, bp1_opposing_reads, non_native_split_bp1, non_native_paired_bp1, te1, te_tagged1, bp_sig, seen_reads, supporting, opposing = get_reads(bp_regions, 'bp1', chrom, bp1, bp2, options, seen_reads, chroms, bp_sig, supporting, opposing)
-    bp2_split_reads, bp2_best_guess, bp2_clipped_bam, bp2_disc_bam, bp2_opposing_reads, non_native_split_bp2, non_native_paired_bp2, te2, te_tagged2, bp_sig, seen_reads, supporting, opposing = get_reads(bp_regions, 'bp2', chrom, bp2, bp1, options, seen_reads, chroms, bp_sig, supporting, opposing)
+    bp1_clipped_bam, bp1_disc_bam, bp1_opposing_reads, alien_integrant1, te_tagged1, bp1_sig, seen_reads, supporting, opposing = get_reads(bp_regions, 'bp1', chrom1, bp1, chrom2, bp2, options, seen_reads, chroms, supporting, opposing)
+    bp2_clipped_bam, bp2_disc_bam, bp2_opposing_reads, alien_integrant2, te_tagged2, bp2_sig, seen_reads, supporting, opposing = get_reads(bp_regions, 'bp2', chrom2, bp2, chrom1, bp1, options, seen_reads, chroms, supporting, opposing)
 
     total_support = len(set(supporting))
     total_oppose = len(set(opposing))
 
+    if chrom1 == chrom2:
+        sv_type = classify_sv(bp1_sig, bp2_sig)
+    else:
+        sv_type = 'TRA'
+
+    print("* Variant classified as %s") % (sv_type)
+
     print("* Found %s reads in support of variant" % total_support)
     print("* Found %s reads opposing variant" % total_oppose)
 
-    af = AlleleFrequency(total_oppose, total_support, purity, chrom)
+    af = AlleleFrequency(total_oppose, total_support, purity, chrom1)
     allele_frequency = af.read_support_af()
 
-    if non_native_split_bp1 or non_native_paired_bp1:
-        print " * Found reads supporting integration of foreign DNA at bp1 (%s split, %s paired)" % (non_native_split_bp1, non_native_paired_bp1)
-
-    if te_tagged1[te1]:
-        print " * %s %s-tagged reads found at bp1" % (te_tagged1[te1], te1)
-
-    if non_native_split_bp2 or non_native_paired_bp2:
-        print " * Found reads supporting integration of foreign DNA at bp2 (%s split, %s paired)" % (non_native_split_bp2, non_native_paired_bp2)
-
-    if te_tagged2[te2]:
-        print " * %s %s-tagged reads found at bp2" % (te_tagged2[te2], te2)
 
 
     # clio = os.path.join(out_dir, 'clipped_reads.bam')
     # disco = os.path.join(out_dir, 'discordant_reads.bam')
     # merge_bams(disco, out_dir, [bp1_disc_bam, bp2_disc_bam])
 
-    svID = '_'.join(map(str, [chrom, bp1, bp2]))
+    svID = '_'.join(map(str, [chrom1, bp1, chrom2, bp2]))
 
     suoout = os.path.join(out_dir, svID + '_supporting.bam')
     opout = os.path.join(out_dir, svID + '_opposing_reads.bam')
@@ -99,38 +113,64 @@ def worker(options):
     merge_bams(suoout, out_dir, [bp1_clipped_bam, bp2_clipped_bam, bp1_disc_bam, bp2_disc_bam])
     merge_bams(opout, out_dir, [bp1_opposing_reads, bp2_opposing_reads])
 
-    print "Breakpoint signatures %s" % (bp_sig)
+    alien1, te1 = assessIntegration(alien_integrant1, te_tagged1, 'bp1')
+    alien2, te2 = assessIntegration(alien_integrant2, te_tagged2, 'bp2')
+    integrants = [alien1, te1, alien2, te2]
 
-    sv_type = classify_sv(bp_sig)
+    if not any(integrants):
+        integrants = None
+
+    return bp1, bp2, allele_frequency, sv_type, integrants
 
 
+def assessIntegration(alien, te, bp_number):
+    alien[None] = 0
+    ak = max(alien, key=alien.get)
 
-    return (chrom, bp1, bp2, allele_frequency)
+    if ak:
+        print " * Found %s reads supporting integration of foreign DNA at %s from source %s" % (alien[ak], bp_number, ak)
+        alien_string = '_'.join(map(str, [bp_number, ak, alien[ak]]))
+    else:
+        alien_string = None
+
+    te[None] = 0
+    tk = max(te, key=te.get)
+
+    if tk:
+        print " * Found %s %s-tagged reads at %s" % (te[tk], tk, bp_number)
+        te_string = '_'.join(map(str, [bp_number, tk, te[tk]]))
+    else:
+        te_string = None
+
+    return alien_string, te_string
 
 
-def get_regions(bam_in, chrom, bp1, bp2, out_dir):
-    slop = find_is_sd(bam_in, 10000)
+def get_regions(bam_in, chrom1, bp1, chrom2, bp2, out_dir, options):
+    if not options.slop:
+        slop = find_is_sd(bam_in, 10000)
+    else:
+        slop = options.slop
+
     samfile = pysam.Samfile(bam_in, "rb")
 
     bp1_bam = os.path.join(out_dir, "bp1_region" + ".bam")
 
     with pysam.AlignmentFile(bp1_bam, "wb", template=samfile) as bp1_region:
-        for read in samfile.fetch(chrom, bp1 - slop, bp1 + slop):
+        for read in samfile.fetch(chrom1, bp1 - slop, bp1 + slop):
             bp1_region.write(read)
 
     bp2_bam = os.path.join(out_dir, "bp2_region" + ".bam")
     with pysam.AlignmentFile(bp2_bam, "wb", template=samfile) as bp2_region:
-        for read in samfile.fetch(chrom, bp2 - slop, bp2 + slop):
+        for read in samfile.fetch(chrom2, bp2 - slop, bp2 + slop):
             bp2_region.write(read)
 
     bps_bam = os.path.join(out_dir, "bp_regs" + ".bam")
     regions = merge_bams(bps_bam, out_dir, [bp1_bam, bp2_bam])
 
-    bpID = '_'.join(map(str, [chrom, bp1-slop, bp2+slop]))
-
+    bpID = '_'.join(map(str, [chrom1, bp1-slop, chrom2, bp2+slop]))
     dups_rem = rmDups(regions, bpID + "_regions.bam", out_dir)
-
     sorted_bam = sort_bam(out_dir, dups_rem)
+    rm_bams([dups_rem])
 
     return sorted_bam, slop
 
@@ -153,11 +193,6 @@ def rmDups(bamfile, outfile, out_dir):
 
             out.write(read)
 
-    try:
-        os.remove(bamfile)
-        os.remove(bamfile + ".bai")
-    except OSError:
-        print("Couldn't remove %s" % bamfile)
-        pass
+    rm_bams([bamfile])
 
     return dups_rem
