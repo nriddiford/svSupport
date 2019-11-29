66c17a0d103b7921a754c04db7901b37ece95459
nriddiford <nickriddiford@me.com>
2019-11-29 14:37:53 +0100

    update variants

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -23,7 +23,8 @@ function getBase(){
 }
 
 bamdir=/Volumes/perso/Analysis/Bwa
-variants=/Users/Nick_curie/Desktop/parserTest/filtered4/summary/merged
+#variants=/Users/Nick_curie/Desktop/parserTest/filtered4/summary/merged
+variants=/Users/Nick_curie/Desktop/parserTest/filtered_delta_16919/summary/merged
 out_dir=/Users/Nick_curie/Desktop/script_test/svSupport/out
 purity=/Users/Nick_curie/Desktop/script_test/svSupport/data/tumour_purity.txt
 script_bin=/Users/Nick_curie/Desktop/script_test/svSupport
